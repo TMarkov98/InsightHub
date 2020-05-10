2cbf36f96b5b03db835cc2f63c5adcfcfc47cd6c
teodor.atanasov.markov@abv.bg <teodor.atanasov.markov@abv.bg>
2020-05-10 17:05:37 +0300

    feat: Added Industry Services
    - Added Create
    - Added Update
    - Added Get
    - Added GetAll
    - Added Delete
    - Added Industry Mapper
    - Added IndustryDTO

---
+++ InsightHub/InsightHub.Services/DTOs/ReportDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsightHub.Services.DTOs
+{
+    public class ReportDTO
+    {
+    }
+}
---
+++ InsightHub/InsightHub.Services/Mappers/IndustryMapper.cs
@@ -0,0 +1,38 @@
+﻿using InsightHub.Models;
+using InsightHub.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsightHub.Services.Mappers
+{
+    public static class IndustryMapper
+    {
+        public static IndustryDTO MapDTOFromString(string name)
+        {
+            return new IndustryDTO
+            {
+                Name = name,
+                CreatedOn = DateTime.UtcNow
+            };
+        }
+    //TODO CreatedOn??
+        public static Industry MapIndusryFromDTO(IndustryDTO iDTO)
+        {
+            return new Industry
+            {
+                Name = iDTO.Name,
+                CreatedOn = iDTO.CreatedOn,
+            };
+        }
+        public static IndustryDTO MapDTOFromIndustry(Industry industry)
+        {
+            return new IndustryDTO
+            {
+                Id = industry.Id,
+                Name = industry.Name,
+                CreatedOn = industry.CreatedOn,
+            };
+        }
+    }
+}
