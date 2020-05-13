de59eb92d2460ea7c9c9f5e8a767dff8e206b6ed
teodor.atanasov.markov@abv.bg <teodor.atanasov.markov@abv.bg>
2020-05-13 21:23:02 +0300

    feat: Tests CreateTag
    - Added Utils
    - Added CreateTag_Should

---
+++ InsightHub/InsightHub.Tests/UnitTests/TagServicesTests/CreateTag_Should.cs
@@ -0,0 +1,38 @@
+﻿using InsightHub.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using InsightHub.Services;
+using System.Threading.Tasks;
+
+namespace InsightHub.Tests.TagServicesTests
+{
+    [TestClass]
+    public class CreateTag_Should
+    {
+        [TestMethod]
+        public async Task ReturnCurrectTag_When_ParamValid()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCurrectTag_When_ParamValid));
+            var tagName = "space";
+
+            using var assertContext = new InsightHubContext(options);
+            var sut = new TagServices(assertContext);
+            var act = await sut.CreateTag(tagName);
+            var result = assertContext.Tags.FirstOrDefault(t => t.Name == tagName);
+            Assert.AreEqual(tagName, result.Name);
+        }
+
+        [TestMethod]
+        public async Task ThrowArgumentException_When_AlreadyExists()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCurrectTag_When_ParamValid));
+            var tagName = "space";
+
+            using var assertContext = new InsightHubContext(options);
+            var sut = new TagServices(assertContext);
+            var act = sut.CreateTag(tagName);
+            await Assert.ThrowsExceptionAsync<ArgumentException>(() => sut.CreateTag(tagName));
+        }
+    }
+}
---
+++ InsightHub/InsightHub.Tests/Utils.cs
@@ -0,0 +1,18 @@
+﻿using InsightHub.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsightHub.Tests
+{
+    public class Utils
+    {
+        public static DbContextOptions<InsightHubContext> GetOptions(string databaseName)
+        {
+            return new DbContextOptionsBuilder<InsightHubContext>()
+                .UseInMemoryDatabase(databaseName)
+                .Options;
+        }
+    }
+}
