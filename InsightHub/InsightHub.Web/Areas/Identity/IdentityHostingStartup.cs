680653734a73859ed3d4e2d32c21039430c7aace
teodor.atanasov.markov@abv.bg <teodor.atanasov.markov@abv.bg>
2020-05-14 18:22:48 +0300

    feat: Adding Identity
    -Added Areas
    -Added UserViews

---
+++ InsightHub/InsightHub.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using InsightHub.Data;
+using InsightHub.Data.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InsightHub.Web.Areas.Identity.IdentityHostingStartup))]
+namespace InsightHub.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
