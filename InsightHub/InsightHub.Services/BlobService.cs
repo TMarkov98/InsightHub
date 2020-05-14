af1af4dd648e56113585d2eeb3b58499d43781fd
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-05-14 20:20:06 +0300

    feat: Added Blob Storage

---
+++ InsightHub/InsightHub.Services/BlobService.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using InsightHub.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightHub.Services
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public BlobService(BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+        }
+
+        
+
+        public async Task<BlobInfo> GetBlobAsync(string name)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("reports");
+            var blobClient = containerClient.GetBlobClient(name);
+            var blobDownloadInfo = await blobClient.DownloadAsync();
+            return new BlobInfo(blobDownloadInfo.Value.Content, blobDownloadInfo.Value.ContentType);
+        }
+
+        public async Task<IEnumerable<string>> ListBlobsAsync()
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("reports");
+            var items = new List<string>();
+            await foreach(var blobItem in containerClient.GetBlobsAsync())
+            {
+                items.Add(blobItem.Name);
+            }
+            return items;
+        }
+
+        public Task UploadContentBlobAsync(string content, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadFileBlobAsync(string filePath, string fileName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("reports");
+            var blobClient = containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(filePath, new BlobHttpHeaders { ContentType = filePath.GetContentType()});
+        }
+        public Task DeleteBlobAsync(string blobName)
+        {
+
+        }
+    }
+}
---
+++ InsightHub/InsightHub.Services/Contracts/IBlobService.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightHub.Services.Contracts
+{
+    public interface IBlobService
+    {
+        Task<BlobInfo> GetBlobAsync(string name);
+        Task<IEnumerable<string>> ListBlobsAsync();
+        Task UploadFileBlobAsync(string filePath, string fileName);
+        Task UploadContentBlobAsync(string content, string fileName);
+        Task DeleteBlobAsync(string blobName);
+    }
+}
