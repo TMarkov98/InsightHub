df769e35b6b052e1465212e82b53ff1844ee0804
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-05-22 13:46:35 +0300

    feat: Added Downloaded Reports to Client Dashboard
    - Added User Service to get downloaded reports
    - Added User Service to get all reports written by user
    - Added Sort/Search functionality to Downloaded Reports view

--- InsightHub/InsightHub.Services/Contracts/IUserServices.cs
+++ InsightHub/InsightHub.Services/Contracts/IUserServices.cs
@@ -6,12 +6,14 @@ namespace InsightHub.Services.Contracts
 {
     public interface IUserServices
     {
-        Task<bool> BanUser(int id, string reason);
+        Task BanUser(int id, string reason);
         Task<List<UserModel>> GetBannedUsers();
         Task<List<UserModel>> GetPendingUsers();
         Task<UserModel> GetUser(int id);
         Task<List<UserModel>> GetUsers();
-        Task<bool> UnbanUser(int id);
+        Task UnbanUser(int id);
         Task<UserModel> UpdateUser(int id, string firstName, string lastName, bool lockoutEnabled, string LockOutReason);
+        Task<List<ReportModel>> GetDownloadedReports(int userId);
+        Task<List<ReportModel>> GetMyReports(int userId);
     }
 }
