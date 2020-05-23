923c0963fd16c4f06d43f48a5d8769f1c1109968
Teodor Markov <teodor.atan.markov@gmail.com>
2020-05-23 21:49:55 +0300

    feat: Added MyReports MySubcriptions for Client
    -Added MyReportsController
    -Added MySubscriptionsController
    -Added SubscribedIndustries View

---
+++ InsightHub/InsightHub.Web/Areas/Client/Controllers/MySubscriptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using InsightHub.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+
+namespace InsightHub.Web.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class MySubscriptions : Controller
+    {
+        private readonly IUserServices _userServices;
+
+        public MySubscriptions(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+        [Authorize]
+        // GET: MySubscriptions 
+        public async Task<IActionResult> Index()
+        {
+            var userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var subscriptions = await _userServices.GetMySubscriptions(userId);
+            return View(subscriptions);
+        }
+
+    }
+}
