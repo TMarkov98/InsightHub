e58eedb64f4a257778c6f8cc47b9d9a053f346e3
Teodor Markov <teodor.atan.markov@gmail.com>
2020-05-26 17:11:30 +0300

    feat: Updated New Unit Tests for TagServices and IndustryServices
    - AddSubscription_Should
    - GetAllIndustries_Should
    - RemoveSubscription_Should

--- InsightHub/InsightHub.Tests/UnitTests/IndustryServicesTests/GetDeletedIndustries_Should.cs
+++ InsightHub/InsightHub.Tests/UnitTests/IndustryServicesTests/GetDeletedIndustries_Should.cs
@@ -33,5 +33,28 @@ namespace InsightHub.Tests.UnitTests.IndustryServicesTests
                 Assert.AreEqual(1, result.Count);
             }
         }
+        [TestMethod]
+        public async Task ReturnDeletedIndustries_WithSearch()
+        {
+            var options = Utils.GetOptions(nameof(ReturnDeletedIndustries_WithSearch));
+            var industry = TestModelsSeeder.SeedIndustry();
+            var industry2 = TestModelsSeeder.SeedIndustry2();
+            industry.IsDeleted = true;
+            industry2.IsDeleted = true;
+
+            using (var arrangeContext = new InsightHubContext(options))
+            {
+                await arrangeContext.Industries.AddAsync(industry);
+                await arrangeContext.Industries.AddAsync(industry2);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using (var assertContext = new InsightHubContext(options))
+            {
+                var sut = new IndustryServices(assertContext);
+                var result = await sut.GetDeletedIndustries("Test");
+                Assert.AreEqual(2, result.Count);
+            }
+        }
     }
 }
--- InsightHub/InsightHub.Tests/UnitTests/TagServicesTests/GetTags_Should.cs
+++ InsightHub/InsightHub.Tests/UnitTests/TagServicesTests/GetTags_Should.cs
@@ -34,6 +34,76 @@ namespace InsightHub.Tests.UnitTests.TagServicesTests
             var act = await sut.GetTags(null, null);
             var result = act.ToArray();
             Assert.AreEqual(firstTag.Name, result[0].Name);
-        } 
+        }
+        [TestMethod]
+        public async Task ReturnCurrectTags_WithSort_When_ParamValid()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCurrectTags_WithSort_When_ParamValid));
+            var firstTag = TestModelsSeeder.SeedTag();
+            var secondTag = TestModelsSeeder.SeedTag2();
+            var thirdTag = TestModelsSeeder.SeedTag3();
+
+            using (var arrangeContext = new InsightHubContext(options))
+            {
+                arrangeContext.Tags.Add(firstTag);
+                arrangeContext.Tags.Add(secondTag);
+                arrangeContext.Tags.Add(thirdTag);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using var assertContext = new InsightHubContext(options);
+            var sut = new TagServices(assertContext);
+            var act = await sut.GetTags("name_desc", null);
+            var result = act.ToArray();
+            Assert.AreEqual(thirdTag.Name, result[0].Name);
+            Assert.AreEqual(secondTag.Name, result[1].Name);
+            Assert.AreEqual(firstTag.Name, result[2].Name);
+        }
+        [TestMethod]
+        public async Task ReturnCurrectTags_WithSearch_When_ParamValid()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCurrectTags_WithSearch_When_ParamValid));
+            var firstTag = TestModelsSeeder.SeedTag();
+            var secondTag = TestModelsSeeder.SeedTag2();
+            var thirdTag = TestModelsSeeder.SeedTag3();
+
+            using (var arrangeContext = new InsightHubContext(options))
+            {
+                arrangeContext.Tags.Add(firstTag);
+                arrangeContext.Tags.Add(secondTag);
+                arrangeContext.Tags.Add(thirdTag);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using var assertContext = new InsightHubContext(options);
+            var sut = new TagServices(assertContext);
+            var act = await sut.GetTags(null, "3");
+            var result = act.ToArray();
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(thirdTag.Name, result[0].Name);
+        }
+        [TestMethod]
+        public async Task ReturnCurrectTags_WithSortAndSearch_When_ParamValid()
+        {
+            var options = Utils.GetOptions(nameof(ReturnCurrectTags_WithSortAndSearch_When_ParamValid));
+            var firstTag = TestModelsSeeder.SeedTag();
+            var secondTag = TestModelsSeeder.SeedTag2();
+            var thirdTag = TestModelsSeeder.SeedTag3();
+
+            using (var arrangeContext = new InsightHubContext(options))
+            {
+                arrangeContext.Tags.Add(firstTag);
+                arrangeContext.Tags.Add(secondTag);
+                arrangeContext.Tags.Add(thirdTag);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using var assertContext = new InsightHubContext(options);
+            var sut = new TagServices(assertContext);
+            var act = await sut.GetTags("name_desc", "Test");
+            var result = act.ToArray();
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(thirdTag.Name, result[0].Name);
+        }
     }
 }
