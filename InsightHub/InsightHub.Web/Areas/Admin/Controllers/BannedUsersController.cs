bd0c95f81a64f63cfd8c6d89f071d7655485cb83
Teodor Markov <teodor.atan.markov@gmail.com>
2020-06-02 14:48:39 +0300

    refactor: Added Search to PendingUsers and BannedUsers

--- InsightHub/InsightHub.Web/Areas/Admin/Controllers/BannedUsersController.cs
+++ InsightHub/InsightHub.Web/Areas/Admin/Controllers/BannedUsersController.cs
@@ -28,6 +28,8 @@ namespace InsightHub.Web.Areas.Admin.Controllers
         public async Task<IActionResult> Index(string search, int? pageNumber)
         {
             var users = await _userServices.GetBannedUsers(search);
+            ViewData["ResultsCount"] = users.Count;
+            ViewData["Search"] = search;
             if (search != null)
             {
                 pageNumber = 1;
